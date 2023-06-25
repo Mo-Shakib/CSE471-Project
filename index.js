20d20c9974accd9d312fd2ff1b80f51296490afb
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2023-06-25 13:44:59 +0600

    database connection

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
 
 app.listen(8800, () => {
     console.log("Backend server is running!");
